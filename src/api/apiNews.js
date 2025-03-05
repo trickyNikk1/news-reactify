c2f3a2f4297c3aba90a3edb32f103f55d3cd9737
Nikita Aristarkhov <aristarhovnikita1@gmail.com>
2025-03-05 18:34:34 +0700

    feat: add categories

--- src/api/apiNews.js
+++ src/api/apiNews.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const BASE_URL = import.meta.env.VITE_NEWS_BASE_API_URL
 const API_KEY = import.meta.env.VITE_NEWS_API_KEY
 
-export const getNews = async (signal, page_number = 1, page_size = 10) => {
+export const getNews = async ({ signal, page_number = 1, page_size = 10, category = 'All' }) => {
   try {
     const response = await axios.get(`${BASE_URL}search`, {
       signal,
@@ -11,6 +11,21 @@ export const getNews = async (signal, page_number = 1, page_size = 10) => {
         apiKey: API_KEY,
         page_number,
         page_size,
+        category,
+      },
+    })
+    return response.data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const getCategories = async (signal) => {
+  try {
+    const response = await axios.get(`${BASE_URL}available/categories`, {
+      signal,
+      params: {
+        apiKey: API_KEY,
       },
     })
     return response.data
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,70 +1,94 @@
 import { useEffect, useState } from "react"
 import { NewsBanner } from "../../components/NewsBanner/NewsBanner"
 import styles from './styles.module.css'
-import { getNews } from "../../api/apiNews"
+import { getCategories, getNews } from "../../api/apiNews"
 import { NewsList } from "../../components/NewsList/NewsList"
 import { Skeleton } from "../../components/Skeleton/Skeleton"
 import { Pagination } from "../../components/Pagination/Pagination"
+import { Categories } from "../../components/Categories/Categories"
 
 export const Main = () => {
-    const [news, setNews] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
-    const [currentPage, setCurrentPage] = useState(1)
-    const totalPages = 10
-    const pageSize = 10
+  const [news, setNews] = useState([])
+  const [categories, setCategories] = useState([])
+  const [selectedCategory, setSelectedCategory] = useState('All')
+  const [isLoading, setIsLoading] = useState(true)
+  const [currentPage, setCurrentPage] = useState(1)
+  const totalPages = 10
+  const pageSize = 10
 
-    useEffect(() => {
-      const controller = new AbortController()
-      const fetchNews = async () => {
-        try{
-          setIsLoading(true)
-          const response = await getNews(controller.signal, pageSize, currentPage)
-          setNews(response.news)
-        }
-        catch (error) {
-          console.error(error)
-        }
-      }
-      fetchNews()
-      return () => {
-        controller.abort()
-      }
-    }, [currentPage])
+  const fetchNews = async (signal, currentPage) => {
+    try{
+      setIsLoading(true)
+      const response = await getNews({signal, page_size: pageSize, page_number: currentPage, category: selectedCategory === 'All' ? null : selectedCategory})
+      setNews(response.news)
+    }
+    catch (error) {
+      console.error(error)
+    }
+  }
 
-    const handleNextPage = () => {
-      if(currentPage < totalPages) {
-        setCurrentPage(currentPage + 1)
-      }
+  useEffect(() => {
+    const controller = new AbortController()
+    fetchNews(controller.signal, currentPage)
+    return () => {
+      controller.abort()
     }
-    
-    const handlePreviousPage = () => {
-      if(currentPage > 1) {
-        setCurrentPage(currentPage - 1)
-      }
+  }, [currentPage, selectedCategory])
+  
+  const fetchCategories = async (signal) => {
+    try{
+      setIsLoading(true)
+      const response = await getCategories(signal)
+      setCategories('All', ...response.categories)
     }
-    
-    const handlePageClick = (pageNumber) => {
-        setCurrentPage(pageNumber)
+    catch (error) {
+      console.error(error)
     }
+  }
 
-    return (
-      <main className={styles.main}>
-        {news.length > 0 && !isLoading ? <NewsBanner item={news[0]}/> : <Skeleton type={'banner'} count={1} />}
-        <Pagination
-          currentPage={currentPage}
-          totalPages={totalPages} 
-          handleNextPage={handleNextPage}
-          handlePreviousPage={handlePreviousPage}
-          handlePageClick={handlePageClick}
-        />
-        {!isLoading ? <NewsList news={news}/> : <Skeleton type={'item'} count={10} />}
-        <Pagination
-          currentPage={currentPage}
-          totalPages={totalPages} 
-          handleNextPage={handleNextPage}
-          handlePreviousPage={handlePreviousPage}
-          handlePageClick={handlePageClick}
-        />
-      </main>
-    )
+  useEffect(() => {
+    const controller = new AbortController()
+    fetchCategories(controller.signal)
+    return () => {
+      controller.abort()
+    }
+  }, [])
+  
+  const handleNextPage = () => {
+    if(currentPage < totalPages) {
+      setCurrentPage(currentPage + 1)
+    }
+  }
+  
+  const handlePreviousPage = () => {
+    if(currentPage > 1) {
+      setCurrentPage(currentPage - 1)
+    }
+  }
+  
+  const handlePageClick = (pageNumber) => {
+      setCurrentPage(pageNumber)
+  }
+
+  return (
+    <main className={styles.main}>
+      <Categories categories={categories} selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}/>
+      {news.length > 0 && !isLoading ? <NewsBanner item={news[0]}/> : <Skeleton type={'banner'} count={1} />}
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages} 
+        handleNextPage={handleNextPage}
+        handlePreviousPage={handlePreviousPage}
+        handlePageClick={handlePageClick}
+      />
+      {!isLoading ? <NewsList news={news}/> : <Skeleton type={'item'} count={10} />}
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages} 
+        handleNextPage={handleNextPage}
+        handlePreviousPage={handlePreviousPage}
+        handlePageClick={handlePageClick}
+      />
+    </main>
+  )
 }
