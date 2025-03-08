0f193802dfcb439589dec814d8812f16a53d6eea
Nikita Aristarkhov <aristarhovnikita1@gmail.com>
2025-03-08 16:16:46 +0700

    feat: add dark theme

--- src/components/PaginationWrapper/PaginationWrapper.tsx
+++ src/components/PaginationWrapper/PaginationWrapper.tsx
@@ -1,13 +1,13 @@
 import { Pagination } from '../Pagination/Pagination'
 import { PaginationProps } from '../../interfaces'
 
-interface Props extends PaginationProps {
+interface Props {
   top?: boolean
   bottom?: boolean
   children: React.ReactNode
 }
 
-export const PaginationWrapper = ({ top, bottom, children, ...paginationProps }: Props) => {
+export const PaginationWrapper = ({ top, bottom, children, ...paginationProps }: Props & PaginationProps) => {
   return (
     <>
       {top && <Pagination {...paginationProps} />}
